9282eb7d7993322a39ad411125f7c5649eda0763
Sean <whiteibescu@gmail.com>
2020-04-04 04:37:46 +0900

    IntergralConversion

---
+++ 3장IntergralConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3장IntergralConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IntergralConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergralConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+            sbyte a = 127;
+        Console. Writeline(a);
+
+            int = b (int)a;
+            Console.Writeline(b);
+
+            int x = 128; // sbyte 의 최대값 127보다 1 큰 수
+       Console.Writeline(x);
+
+            sbyte y = (sbyte)x;
+        Console.WriteLine(Y);
+       
+        }
+    }
+}
