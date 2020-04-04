cc3d16e40d09552bda53f8bca102b93d81741532
Sean <whiteibescu@gmail.com>
2020-04-04 16:28:41 +0900

    오타 수정

--- IntergralConversion/Program.cs
+++ IntergralConversion/Program.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IntergralConversion
 {
-    class Program
+    class MainApp
     {
-        static void Main(string[] args)
+        static void Main(string[] args);
+        {
             sbyte a = 127;
-        Console. Writeline(a);
+            Console. Writeline(a);
 
             int = b (int)a;
             Console.Writeline(b);
 
             int x = 128; // sbyte 의 최대값 127보다 1 큰 수
-       Console.Writeline(x);
+            Console.Writeline(x);
 
             sbyte y = (sbyte)x;
-        Console.WriteLine(Y);
+            Console.WriteLine(Y);
        
         }
     }
