2b748beefe819130e2f0e2d4e848c9cab35cd3c2
Sean <whiteibescu@gmail.com>
2020-04-05 21:48:18 +0900

    object

--- Object/Program.cs
+++ Object/Program.cs
@@ -9,7 +9,7 @@ namespace Object
             object a = 123;
             object b = 3.141592653589793238462643383279m;
             object c = true;
-            object d = "안녕하세요."; //object
+            object d = "안녕하세요."; //object //object
 
             Console.WriteLine(a);
             Console.WriteLine(b);
