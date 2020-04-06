29c09d5b1e61adc81c6ab073bb4c28f5c694e26d
Sean <whiteibescu@gmail.com>
2020-04-06 12:41:24 +0900

    enum2

---
+++ enum2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace enum2
+{
+    class MainApp
+    {
+        enum DialogResult { YES, NO , CANCEL, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            Console.WriteLine(result == DialogResult.YES);
+            Console.WriteLine(result == DialogResult.NO);
+            Console.WriteLine(result == DialogResult.CANCEL);
+            Console.WriteLine(result == DialogResult.CONFIRM);
+            Console.WriteLine(result == DialogResult.OK);
+            Console.ReadLine();
+        }
+    }
+}
