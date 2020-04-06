ddc5ff7d784000968c69c10bb04a8ffd89675f5d
Sean <whiteibescu@gmail.com>
2020-04-06 14:10:12 +0900

    EnglishSymbolName

---
+++ EnglishSymbolNames/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishSymbolNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+/*
+~ tilde 
+` back tick
+! exclamation Mark
+@at
+# sharp
+$ dollar
+% percent
+^ circumplex
+& ampersand
+* asterisk
+? question mark
+= equal sign
+- dash
+() parenthesis 괄호
+_ underscore
+{} curly brackets
+[] square brackets
+<> pointy brackets 
+/ slash
+\ back slash
+: colon
+; semi colon
+'' quote
+"" double quote
+*/
+        }
+    }
+}
