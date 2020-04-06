75479f38bdee1f72e7b62070568fbe0d1c1f7efe
Sean <whiteibescu@gmail.com>
2020-04-06 12:44:19 +0900

    enum3

---
+++ enum3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace enum3
+{
+    class MainApp
+    {
+        enum DialogResult { YES = 10, NO, CANCEL, CONFIRM = 50, OK }
+
+        static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+            Console.ReadLine();
+        }
+    }
+}
