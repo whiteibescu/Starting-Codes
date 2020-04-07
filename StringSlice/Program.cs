ae0548cf4e07e87e5a0b94c98bc7f8830aaedab6
Sean <whiteibescu@gmail.com>
2020-04-07 09:15:32 +0900

    StringSlice

---
+++ StringSlice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace StringSlice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good Morning.";
+
+            WriteLine(greeting.Substring(0, 5)); //"Good"
+            WriteLine(greeting.Substring(5)); //"morning"
+
+            WriteLine();
+
+            string[] arr = greeting.Split(
+                new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
+        }
+    }
+}
