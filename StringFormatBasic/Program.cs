2826154dfbcf9dbd72bbf7a8e9df88446985be92
Sean <whiteibescu@gmail.com>
2020-04-07 10:49:27 +0900

    StringFormatBaxic

---
+++ StringFormatBasic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace StringFormatBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0, -20}{1,-15}{2, 30}";
+
+            WriteLine(fmt, "Publisher", "Author", "Title");
+            WriteLine(fmt, "Marvel", "Stan Lee", "Iron Man");
+            WriteLine(fmt, "Hanbit", "Sanghyun Park", "C# 7.0 Programming");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+        }
+    }
+}
