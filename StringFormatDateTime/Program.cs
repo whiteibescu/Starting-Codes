aa66daeaf55b891ee39a147a54466370f2fc3a88
Sean <whiteibescu@gmail.com>
2020-04-07 23:10:46 +0900

    StringFormatDateTime

---
+++ StringFormatDateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+namespace StringFormatDateTime
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+
+            WriteLine("12시간 형식: {0:yyyy-MM-dd tt hh:mmss (ddd)}", dt);
+            WriteLine("12시간 형식: {0:yyyy-MM-dd HH:mm:ss (ddd)}", dt);
+
+            CultureInfo ciKo = new CultureInfo("ko-KR");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (dddd)", ciKo));
+
+
+        }
+    }
+}
