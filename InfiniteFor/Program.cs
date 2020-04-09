c304cd9bb2a5845cba51e9b3bf98787f4af4bbd2
Sean <whiteibescu@gmail.com>
2020-04-10 06:50:52 +0900

    무한대

---
+++ InfiniteFor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InfiniteFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (; ; )
+                Console.WriteLine(i++);
+        }
+    }
+}
