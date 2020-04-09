bcecfab6df67d8d498f6aef5169287889c475b86
Sean <whiteibescu@gmail.com>
2020-04-10 06:52:12 +0900

    무한대

---
+++ InfiniteWhile/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InfiniteWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+                Console.WriteLine(i++);
+        }
+    }
+}
