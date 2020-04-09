db612ece639a851f535ac329b831dd1d678c97d0
Sean <whiteibescu@gmail.com>
2020-04-10 06:59:42 +0900

    Fun Coding with Break

---
+++ Break/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("계속할까요?(예/아니오 : ");
+                string answer = Console.ReadLine();
+
+                if (answer == "아니오")
+                    break;
+            }
+        }
+    }
+}
