2fbf0d8382ca788dd5d6b2486ee46d184fddf8ca
Sean <whiteibescu@gmail.com>
2020-04-09 22:36:06 +0900

    While Example

---
+++ While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+           
+            while (i > 0);
+                        
+            {
+                Console.WriteLine($"i : {i--}");
+            }            
+        }
+    }
+}
