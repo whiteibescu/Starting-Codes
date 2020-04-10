f4cf9f4f9bd839c1b1504642f97a71881c1e302a
Sean <whiteibescu@gmail.com>
2020-04-11 00:45:40 +0900

    ex

---
+++ SwapExample2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SwapExample2
+{
+    class Program
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+}
