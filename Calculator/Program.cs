e9ea72860d20740793b7d7194551b181a75df35b
Sean <whiteibescu@gmail.com>
2020-04-11 00:05:38 +0900

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+          public static int Plus(int a, int b)
+            {
+            return a + b;
+            }
+
+            public static int Minus(int a, int b)
+            {
+                return a -b;
+            }
+                
+        
+        class MainApp
+        {
+            public static void Main()
+            {
+                int result = Calculator.Plus(3, 4);
+                Console.WriteLine(result);
+
+                result = Calculator.Minus(5, 2);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
