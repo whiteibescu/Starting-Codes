6a9d86676efa75872caca9957ed5de57ac7d90b6
Sean <whiteibescu@gmail.com>
2020-04-11 02:22:31 +0900

    UsingOut 복습

--- UsingOut/Program.cs
+++ UsingOut/Program.cs
@@ -4,13 +4,13 @@ namespace UsingOut
 {
     class MainApp
     {
-        static void Divide(int a, int b, out int quotient, out int remainder)
+        static void Divide(int a, int b, out int quotient, out int remainder) 
         {
             quotient = a / b;
             remainder = a % b;
         }
 
-        static void Main(string[] args)
+        static void Main(string[] args) //다음은 out 키워드를 이용하여 Divide() 메소도를 호출하는 코드입니다.
         {
             int a = 20;
             int b = 3;
