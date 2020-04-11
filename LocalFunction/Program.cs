07f6cec64c6c0bd1ae13a6827f654eb739826ffd
Sean <whiteibescu@gmail.com>
2020-04-12 01:57:11 +0900

    오류

--- LocalFunction/Program.cs
+++ LocalFunction/Program.cs
@@ -2,7 +2,7 @@
 
 namespace LocalFunction
 {
-    class Program
+    class MainApp
     {
         static void ToLowerString(string input)
         {
@@ -10,18 +10,23 @@ namespace LocalFunction
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = ToLowerChar(i);
-            }
-        }
+            } }
 
         char ToLowerChar(int i)
         {
-            if (Array[i] < 65 || Array[i] > )
+            if (arr[i] < 65 || arr[i] > 90) // A ~ Z의 ASCII 값 : 65~90)
+                return arr[i];
+            else // a ~ z의 ASCII 값 : 97 ~ 122
+                return (char)(arr[i] + 32);
         }
 
-
+        return new string (arr);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ToLowerString("Hello!"));
+            Console.WriteLine(ToLowerString("Good Morning"));
+            Console.WriteLine(ToLowerString("This is C#."));
         }
     }
 }
