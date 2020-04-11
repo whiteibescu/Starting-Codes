cf144a2471c66705e1301412d075336f503e9fc2
Sean <whiteibescu@gmail.com>
2020-04-12 01:49:49 +0900

    복습

---
+++ LocalFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LocalFunction
+{
+    class Program
+    {
+        static void ToLowerString(string input)
+        {
+            var arr = input.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = ToLowerChar(i);
+            }
+        }
+
+        char ToLowerChar(int i)
+        {
+            if (Array[i] < 65 || Array[i] > )
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
