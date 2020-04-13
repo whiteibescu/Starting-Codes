373b6a6da9e5603fb3a21f8dd26b99dad7d8cb4f
Sean <whiteibescu@gmail.com>
2020-04-13 09:58:37 +0900

    연습문제

---
+++ Chapter2Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter2Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            
+            //이곳에 사각형의 넓이를 계산하고
+            //출력하는 루틴을 추가하세요.
+        }
+    }
+}
