c2743f620bb29a3e36745c3575b65a8d1e37011a
Sean <whiteibescu@gmail.com>
2020-04-13 10:03:54 +0900

    연습문제

---
+++ Chapter5Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+// 4.다음과 같이 사용자로부터 입력받은 횟수만큼 별을 반복 출력하는 프로그램을 작성하세요. 
+// 단, 입력받은 수가 0보다 작거나 같을 경우 "0보다 같거나 작은 숫자는 사용할 수 없습니다."라는 메시지를 띄우고 프로그램을 종료합니다.
