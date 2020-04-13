420273e3e0c0a2da2cf2724c2807e9e09d98941a
Sean <whiteibescu@gmail.com>
2020-04-14 00:03:30 +0900

    수정

---
+++ Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Cat
+    {
+        public Cat()
+        {
+            Name = "";
+            Color = "";
+        }
+
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
