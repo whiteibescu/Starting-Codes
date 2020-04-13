f9c3b67d1c04450294d3fa10ecf47c120d970ffa
Sean <whiteibescu@gmail.com>
2020-04-13 10:03:35 +0900

    연습문제

---
+++ Chapter5Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter5Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}//다음과 같은 결과를 출력하는 프로그램을 for 문을 이용하여 작성하세요.
+/*
+
+
+*****
+****
+***
+**
+*
+
+
+*/
