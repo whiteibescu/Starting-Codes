bab4d181d9f98ad50e350e7c59ca980451f0a8ff
Sean <whiteibescu@gmail.com>
2020-04-14 21:24:35 +0900

    오류

---
+++ This/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace This
+{
+    class Program
+    {
+        private string Name;
+        private string Position;
+
+        public void SetTname(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPosition()
+        {
+            this.Position = Position;
+        }
+    }
+
+    class  MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh)");
+            pooh.SetPostition("Waiter");
+            Console.WriteLine($"{pooh.Getname()} {pooh.Position()}");
+
+            Employee tigger = new Employee();
+            tigger.Setname("Tigger");
+            tigger.Setposition("Cleaner");
+            Console.WriteLine($"{tiger,GetName()} {tigger.GetPosition()}");
+        }
+    }
+}
