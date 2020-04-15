af80cfdd5537a306f4d13d9265d8fd80613a87fb
Sean <whiteibescu@gmail.com>
2020-04-15 17:33:55 +0900

    WantToOverride

---
+++ SealedMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    class Base
+    {
+        public virtual void SealMe()
+        {
+        }
+    }
+
+    class Derived : Base {
+        public sealed override void SealMe()
+        {
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        public override void SealMe()
+        {
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+//이 예제 코드의 실행 결과는 없습니다. 컴파일 에러가 납니다.
