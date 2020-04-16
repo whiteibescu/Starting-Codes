c17ac541f89523b16de119eff38d1bd70b91e394
Sean <whiteibescu@gmail.com>
2020-04-17 02:57:24 +0900

    튜플

---
+++ Tuple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tuple
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 명명되지 않은 튜플
+            var a = ("슈퍼맨", 9999);
+            Console.WriteLine($"{a.Item1}, {a.Item2}");
+
+            // 명명된 튜플
+            var b = (Name: "박성현", Age: 17);
+            Console.WriteLine($"{b.Name}, {b.Age}");
+
+            // 분해
+            var (name, age) = b; // (var name, var age) = b;
+            Console.WriteLine($"{name}, {age}");
+
+            // 명명된 튜플 = 명명되지 않은 튜플
+            b = a;
+            Console.WriteLine($"{b.Name}, {b.Age}");
+        }
+    }
+}
+//튜플(Tuple) = 여러 필드를 담을 수 있는 구조체입니다.
