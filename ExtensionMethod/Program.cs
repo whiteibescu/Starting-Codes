7d2c6f7bc46f17bc0e087aa704ccd3f6c72d613c
Sean <whiteibescu@gmail.com>
2020-04-16 19:53:49 +0900

    오류

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ExtensionMethod;
+
+namespace ExtensionMethod
+{
+    public static class IntegerExtension
+    public static int Square(this int myInt)
+    {
+        return myInt * my Int;
+    }
+
+    public static int Power(this int myInt, int exponent)
+    {
+        int result = myInt;
+        for (int i = 1; i < exponent; i++)
+            result = result * myInt;
+
+        return result;
+    }
+}
+
+namespace ExthensionMethod
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"3^2 : {3.Square()}");
+            Console.WriteLine($"3^4 : {3.Power(4)}");
+            Console.WriteLine($"2^10 : {2.Power(10)}");
+        }
+    }
+}
+
+
