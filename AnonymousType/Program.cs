6385b59d04b49ed97bc3c167dac724d4938b087f
Sean <whiteibescu@gmail.com>
2020-04-18 05:32:55 +0900

    9.5 무명형식

---
+++ AnonymousType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "박상현", Age = 123 };
+            Console.WriteLine($"Name:{a.Name}, AGe:{a.Age}");
+
+            var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60 } };
+
+            Console.WriteLine($"Subject:{b.Subject}, Scores: ");
+            foreach (var score in b.Scores)
+                Console.WriteLine($"{score}");
+
+            Console.WriteLine();
+        }
+    }
+}
+//무명형식
