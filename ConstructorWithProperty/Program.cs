fa901f2e9e44baa3f51fab1e3decfbfdc355caa9
Sean <whiteibescu@gmail.com>
2020-04-18 05:21:25 +0900

    프로퍼티와 생성자

--- ConstructorWithProperty/Program.cs
+++ ConstructorWithProperty/Program.cs
@@ -2,11 +2,44 @@
 
 namespace ConstructorWithProperty
 {
-    class Program
+    class BirthdayInfo
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "서현",
+                Birthday = new DateTime(1991, 6, 28)
+            };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+
         }
     }
 }
+//프로퍼티와 생성자
