a5009b28a7ca34ab5b8c88c678a12aeb6997696a
Sean <whiteibescu@gmail.com>
2020-04-17 12:30:30 +0900

    오류

---
+++ Interface/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace Interface
+{
+    interface ILogger
+    {
+        void WritwLong(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(
+                "{0}, {1}",
+                DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(Path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0] {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void start()
+        {
+            while (true)
+                {
+                Console.WriteLine("온도를 입력해주세요.: ");
+                string temperature = Console.ReadLine();
+                if (temperature == "")
+                    break;
+
+                logger.WriteLog("현재 온도 : " + temperature);
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor monitor = new ClimateMonitor(
+                new FileLogger("MyLog.txt"));
+
+            monitor.start();
+        }
+    }
+}
