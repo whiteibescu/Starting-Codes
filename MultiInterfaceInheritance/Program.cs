a82b985131ac36e9836e070f1bccce4782d79e7e
Sean <whiteibescu@gmail.com>
2020-04-17 17:50:49 +0900

    오류

---
+++ MultiInterfaceInheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MultiInterfaceInheritance
+{
+    class IRunnable
+    {
+        void Run();
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+
+        public void FLy()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.FLy();
+
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = car as IFlyable;
+            flyable.Fly();
+        }
+    }
+}
