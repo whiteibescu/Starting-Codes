ff7dc68a02c07d2b73237142dc17e5142a2bfc8d
Sean <whiteibescu@gmail.com>
2020-04-19 01:12:01 +0900

    정답

---
+++ Chapter9ExerciseAnswer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+namespace Chapter9ExerciseAnswer
+{
+    class NameCard
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+    }
+    class MainApp
+    {
+        public static void Main()
+        {
+            NameCard namecard = new NameCard() { name = "상현", age = 24 };
+            WriteLine("나이 : {0}", namecard.age);
+            WriteLine("이름 : {0}", namecard.name);
+        }
+    }
+}
