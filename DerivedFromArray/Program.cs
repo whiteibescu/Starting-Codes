5d0c5cf40e6ebf2b022eacfb6bb7bca56139638e
Sean <whiteibescu@gmail.com>
2020-04-19 22:22:46 +0900

    알아두면 삶이 윤택해지는 System.Array

---
+++ DerivedFromArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DerivedFromArray
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 30, 20, 7, 1 };
+            Console.WriteLine($"Type of array : {array.GetType()}");
+            Console.WriteLine($"Base type of array : {array.GetType().BaseType}");
+        }
+    }
+}
