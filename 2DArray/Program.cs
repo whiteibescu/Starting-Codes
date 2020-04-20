26c5373f9793ff79542c41356a61c176c098586d
Sean <whiteibescu@gmail.com>
2020-04-20 12:13:40 +0900

    2차원 배열

--- 2DArray/Program.cs
+++ 2DArray/Program.cs
@@ -17,13 +17,31 @@ namespace _2DArray
                 Console.WriteLine();
             }
             Console.WriteLine();
-        }
 
-        int[,] arr2 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
 
-        for (int i = 0; i < arr2.GetLength(0); i++)
+            int[,] arr2 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < arr2.GetLength(0); i++)
             {
-            for (int j = 0; j < arr2.GetLength)
+                for (int j = 0; j < arr2.GetLength(1); j++)
+                {
+                    Console.Write($"[{i}, {j}] : {arr2[i, j]}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
 
-    }
+            int[,] arr3 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < arr3.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"[{i}, {j}] : {arr3[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }        
+    } 
 }
