242f9f3cfd974d01d35100764c87e4328d7f7bf3
Sean <whiteibescu@gmail.com>
2020-04-21 00:06:13 +0900

    10장 연습문제

---
+++ Chapter10Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+
+1.다음 배열 선언 문장 중 올바르지 않는 것을 고르세요.
+int[] array = new string[3]{ "안녕", "Hello", "Halo" };
+int[] array = new int[3]{1, 2, 3};
+int[] array = new int[]{1, 2, 3};
+int[] array = {1, 2, 3};
+
+2.두 행렬의 곱은 다음과 같이 계산합니다.
+
+A= a b   B = e f 
+   c d       c h
+
+AXB = a*e+b*g a*xf+b*h
+      c*e+d*g c*f+d*h
+
+다음 두 행렬 A와 B의 곱을 2차원 배열을 이용하여 계산하는 프로그램을 작성하세요.
+
+A = 3 2    B= 9 2
+    1 4       1 7
+
+3.다음 코드의 출력 결과는 무엇일까요?
+Stack stack =  new Stack();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+
+while (stack.Count > 0)
+Console.WriteLine(stack.Pop());
+
+4.다음 코드의 출력 결과는 무엇일까요?
+Queue que = new Queue();
+que.Enqueue(1);
+que.Enqueue(2);
+que.Enqueue(3);
+que.Enqueue(4);
+que.Enqueue(5);
+
+while (que.Count > 0)
+Console.WriteLine(que.Dequeue());
+
+5.다음과 같은 결과를 출력하다록 아래의 코드를 완성하세요.
+회사 : Microsoft
+URL : www.microsoft.com
+
+Hastablke ht = new Hastable();
+         = "Microsoft " ;
+
+
+*/
