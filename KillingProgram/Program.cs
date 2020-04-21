39d00b4f91da7f1b3e2cfe2711b91f69651bc4ad
Sean <whiteibescu@gmail.com>
2020-04-22 02:30:17 +0900

    예외

---
+++ KillingProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KillingProgram
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i <5; i++)
+            {
+                Console.WriteLine(arr[i]); //i가 "배열의 크기가 -1"을 넘어서면 예외를 일으키고 종료. 이후의 코드들은 더 이상 실행하지 않음
+            }
+            Console.WriteLine("종료");
+        }
+    }
+}
