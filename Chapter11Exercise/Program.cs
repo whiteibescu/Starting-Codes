cb98db7f560ed8932f0a600cf1f14196565759db
Sean <whiteibescu@gmail.com>
2020-04-22 02:25:16 +0900

    정답

--- Chapter11Exercise/Program.cs
+++ Chapter11Exercise/Program.cs
@@ -14,9 +14,15 @@ queue2.Enqueue(10);
 queue2.Enqueue("한글");
 queue2.Enqueue(3.14);
 
+
+정답 ====== 첫번째 Queue는 모든형에 대한 값을 받을수있다.
+
+            두번째는 int 형으로 할당했기 때문에 10이라는 값을 제외한 나머지 변수들은 int형이 아니라서 불가합니다.
+
+
 2.다음 코드에서 1)에 들어갈 내용은 무엇입니까?
 
-Dictionary<>
+Dictionary<string> dic = new Dictionary<string>();
 
 dic["하나"] = "one"
 dic["둘"] = "two"
