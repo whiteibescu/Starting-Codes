df5ef27fbe79fafd1892890682cd17b8008605dd
Sean <whiteibescu@gmail.com>
2020-04-22 02:23:05 +0900

    11장 연습문제

---
+++ Chapter11Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+
+1. 다음 코드에서 문제를 찾고, 그 원인을 설명하세요.
+
+using System.Collections.Generic;
+
+Queue queue = new Queue();
+queue.Enqueue(10);
+queue.Enqueue("한글");
+Queue.Enqueue(3.14);
+
+Queue<int> queue2 = new Queue<int>();
+queue2.Enqueue(10);
+queue2.Enqueue("한글");
+queue2.Enqueue(3.14);
+
+2.다음 코드에서 1)에 들어갈 내용은 무엇입니까?
+
+Dictionary<>
+
+dic["하나"] = "one"
+dic["둘"] = "two"
+dic["셋"] = "three"
+dic["넷"] = "four"
+dic["다섯"] = "five"
+
+Console.WriteLine(Dictionary["하나"]);
+Console.WriteLine(Dictionary["둘"]);
+Console.WriteLine(Dictionary["셋"]);
+Console.WriteLine(Dictionary["넷"]);
+Console.WriteLine(Dictionary["다섯"]);
+
+*/
