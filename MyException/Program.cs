4834923155bfe67bc36f0cee58bbfbd5357ab14b
Sean <whiteibescu@gmail.com>
2020-04-23 02:38:35 +0900

    오류

--- MyException/Program.cs
+++ MyException/Program.cs
@@ -27,8 +27,40 @@ namespace MyException
     }
 
     class MainApp
-        static uint MergeARGB(uint alpha, uint red, uint green, uint blue)
     {
-        uint[] args = new uint[] { alphha}
+        static uint MergeARGB(uint alpha, uint red, uint green, uint blue)
+        {
+            uint[] args = new uint[] { alpha, red, green, blue };
+
+            foreach (uint arg in args)
+            {
+                if (arg > 255)
+                    throw new InvalidArgumentException()
+                    {
+                        Argument = arg,
+                        Range = "0~255"
+                    };
+            }
+
+            return (alpha << 24 & 0xFF000000) |
+                (red << 16 & 0x00FF0000) |
+                (green << 8 & 0x0000FF00) |
+                (blue & 0x000000FF);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("0x{0:X}", MergeARGB(255, 111, 111, 111));
+                Console.WriteLine("0x{0:X}", MergeARGB(1, 65, 192, 128));
+                Console.WriteLine("0x{0:X}", MergeARGB(0, 255, 255, 300));
+            }
+            catch (IndexOutOfRangeException e)
+            { 
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Argument:{e.Argument}, Range:{e.Range}");
+            }
+        }
     }
 }
