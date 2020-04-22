8cb522066e34d0651bfb432d3e9d9bed1b8ee979
Sean <whiteibescu@gmail.com>
2020-04-23 02:18:07 +0900

    MyException

---
+++ MyException/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyException
+{
+    class InvalidArgumentException : Exception
+    {
+        public InvalidArgumentException()
+        {
+        }
+
+        public InvalidArgumentException(string message)
+            : base(message)
+        {
+        }
+
+        public object Argument
+        {
+            get;
+            set;
+        }
+
+        public string Range
+        {
+            get;
+            set;
+        }
+    }
+
+    class MainApp
+        static uint MergeARGB(uint alpha, uint red, uint green, uint blue)
+    {
+        uint[] args = new uint[] { alphha}
+    }
+}
