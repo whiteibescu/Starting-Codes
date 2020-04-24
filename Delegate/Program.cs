0a63648cbd8ce97eae2b9cb34e38a60200761e7e
Sean <whiteibescu@gmail.com>
2020-04-24 09:52:45 +0900

    미완성

---
+++ Delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate int MyDelegate(int a, int b);
+    
+        class Calculator
+        {
+            public int  Plus(int a, int b)
+            {
+            return a + b
+        }
+
+        }
+    
+}
