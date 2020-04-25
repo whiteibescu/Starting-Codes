8b90f9903f6c008f2133db8118658e0f1061c9bd
Sean <whiteibescu@gmail.com>
2020-04-26 00:53:19 +0900

    오류

---
+++ DelegateChains/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DelegateChains
+{
+    delegate void Notify(string message); //Notify 대리자 선언
+    
+        class Notifier
+        {
+        public Notify EventOccured;
+        }
+
+    class EventListener
+    {
+        private string name;
+        public EventListener(string name)
+        {
+            this.name = name;
+        }
+
+        public SomethingHappend(string message)
+        {
+            Console.WriteLine($"{name.SomethingHappend: {message}");
+        }   
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            EventListener listener1 = new EventListener("Listener1");
+            EventListener listener2 = new EventListener("Listener2");
+            EventListener listener3 = new EventListener("Listener3");
+
+            notifier.EventOccured += listener1.SomethingHappend;
+            notifier.EventOccured += listener1.SomethingHappend;
+            notifier.EventOccured += listener1.SomethingHappend;
+            notifier.EventOccured("You've got mail.");
+
+            Console.WriteLine();
+
+            notifier.EventOccured = new Notify(listener2.SomethingHappend);
+            notifier.EventOccured("Download complete.");
+
+            Console.WriteLine();
+
+            Notify notify1 = new Notify(listener2.SomethingHappend)
+                + new Notify(listener2.SomethingHappend);
+            notifier.EventOccured("Nuclear launch detected.");
+
+            Console.WriteLine();
+
+            Notify notify1 = new Notify(listener1.SomethingHappend);
+            Notify notify2 = new Notify(listener2.SomethingHappend);
+
+            notifier.EventOccured =
+                (Notify)Delegate.Combine(notify1, notify2);
+            notifier.EventOccured("Fire!!");
+
+            Console.WriteLine();
+
+            notifier.EventOccured =
+                (Notify)Delegate.Remove(notifier.EventOccured, notify2);
+            notifier.EventOccured("RPG!");
+        }
+    }    
+}
