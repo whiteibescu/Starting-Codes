fc1802c016a1546e6e6a71015721416957ac3260
Sean <whiteibescu@gmail.com>
2020-04-27 22:08:01 +0900

    수정필요

--- Join/Program.cs
+++ Join/Program.cs
@@ -21,8 +21,11 @@ class Mainapp
     {
         Profile[] arrProfile =
         {
-            new Profile(){Name="정우성", Height="186"},
-            new Profile(){nameof="김태희", Height="158"},
+                new Profile() { Name = "정우성", Height = 186 },
+                new Profile() { Name = "김태희", Height = 158 },
+                new Profile() { Name = "고현정", Height = 172 },
+                new Profile() { Name = "이문세", Height = 178 },
+                new Profile() { Name = "하하", Height = 171 },
         }
     }
 }
