4efff0b388326c2e5567b378cfda7696aa1e5776
Sean <whiteibescu@gmail.com>
2020-04-29 20:04:56 +0900

    16장 연습문제

---
+++ Chapter16Exercise/Program.cs
@@ -0,0 +1,11 @@
+﻿//1. 다음 코드 중에서 올바로 동작하지 않는 것을 고르세요.
+
+using System;
+
+Type t = myObject.GetType();
+Type t = typeof( "int" );
+Type t = Type.GetType(int);
+Type t = Type.GetType("System.Int32");
+
+
+// 2.애트리뷰트와 주석의 차이는 무엇입니까?
