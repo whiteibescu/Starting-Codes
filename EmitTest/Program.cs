7794514fd9f4e319ec03b2d3f17d7744be477879
Sean <whiteibescu@gmail.com>
2020-04-29 12:31:13 +0900

    오류

--- EmitTest/Program.cs
+++ EmitTest/Program.cs
@@ -15,7 +15,31 @@ namespace EmitTest
 
             ModuleBuilder newModule = newAssembly.DefineDynamicModule(
                 "Calculator");
-            TypeBuilder
+            TypeBuilder newType = newModule.DefineType("Sum1To100");
+
+            MethodBuilder newMethod = newType.DefineMethod(
+                "Caclulate",
+                MethodAttributes.Public,
+                typeof(int),
+                new Type[0]);
+
+            ILGenerator generator = newMethod.GetILGenerator();
+
+            generator.Emit(OpCodes.Ldc_I4, 1);
+
+            for (int i = 2; i <= 100; i++)
+            {
+                generator.Emit(OpCodes.Ldc_I4, i);
+                generator.Emit(OpCodes.Add);
+            }
+
+            generator.Emit(OpCodes.Ret);
+            newType.CreateType();
+
+            object sum1To100 = Activator.CreateInstance(newType);
+            MethodInfo Caculate = sum1To100.GetType().
+                GetMethod("Calculate");
+            Console.WriteLine(Calculate.Invoke(sum1To100, null));
         }
     }
 }
