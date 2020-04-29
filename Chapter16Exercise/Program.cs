d8d7e8296cb34934741be597a5c91e13d6762438
Sean <whiteibescu@gmail.com>
2020-04-30 03:01:13 +0900

    확실한 답 아님

--- Chapter16Exercise/Program.cs
+++ Chapter16Exercise/Program.cs
@@ -3,9 +3,11 @@
 using System;
 
 Type t = myObject.GetType();
-Type t = typeof( "int" );
+Type t = typeof( "int" );  //작동 안됨
 Type t = Type.GetType(int);
 Type t = Type.GetType("System.Int32");
 
 
 // 2.애트리뷰트와 주석의 차이는 무엇입니까?
+//주석은 사용자가 쓰고 읽는 정보 ex 메모
+//애트리뷰트는 사용자가 쓰고 컴퓨터가 읽는다.
