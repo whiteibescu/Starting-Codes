0a2593b9f591d3629de1194f1916de8e7a157852
Sean <whiteibescu@gmail.com>
2020-04-29 19:12:05 +0900

    수정

---
+++ CallerInfo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CallerInfo
+{
+    public class static class Trace
+    {
+        public static void WriteLine(string message,
+            [CallerFilePath] string file = "",])
+    }
+}
