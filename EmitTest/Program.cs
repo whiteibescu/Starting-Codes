acde550592f1bc4eb5623a3628259095050201e9
Sean <whiteibescu@gmail.com>
2020-04-29 12:08:32 +0900

    EmitTest

---
+++ EmitTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace EmitTest
+{
+    public class MainApp
+    {
+        public static void Main()
+        {
+            AssemblyBuilder newAssembly =
+                AppDomain.CurrentDomain.DefineDynamicAssembly(
+                    new AssemblyName("CalculatorAssembly"),
+                    AssemblyBuilderAccess.Run);
+
+            ModuleBuilder newModule = newAssembly.DefineDynamicModule(
+                "Calculator");
+            TypeBuilder
+        }
+    }
+}
