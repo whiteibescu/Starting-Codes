9b2409c3d2f77497f5288af073c04ac2985d1e2d
Sean <whiteibescu@gmail.com>
2020-04-29 19:48:11 +0900

    오류

--- CallerInfo/Program.cs
+++ CallerInfo/Program.cs
@@ -6,6 +6,20 @@ namespace CallerInfo
     public class static class Trace
     {
         public static void WriteLine(string message,
-            [CallerFilePath] string file = "",])
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0,
+            [CallerMemberName] string number = "")
+        {
+            Console.WriteLine(
+                $"{file}(Line:{line}) {member}: {message}");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Trace.WriteLine("즐거운 프로그래밍!");
+        }
     }
 }
