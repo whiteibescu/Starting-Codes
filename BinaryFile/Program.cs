5034249dc51b8c6872886053db09395a456a81bf
Sean <whiteibescu@gmail.com>
2020-05-01 00:31:58 +0900

    BinaryFile

---
+++ BinaryFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
