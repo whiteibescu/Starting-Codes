6bc0508edc0eb6b190282cd7d258e501dea17fd3
Sean <whiteibescu@gmail.com>
2020-04-30 21:23:45 +0900

    Dir

--- Dir/Program.cs
+++ Dir/Program.cs
@@ -16,6 +16,29 @@ namespace Dir
 
             Console.WriteLine($"{directory} directory Info");
             Console.WriteLine("- Directories : :");
+            var directories = (from dir in Directory.GetDirectories(directory)
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                   Name = info.Name,
+                                   Attributes = info.Attributes
+                               }).ToList();
+
+            foreach (var d in directories)
+                Console.WriteLine($"{d.Name} : {d.Attributes}");
+
+            Console.WriteLine("- Files :");
+            var files = (from file in Directory.GetFiles(directory)
+                         let info = new FileInfo(file)
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         }).ToList();
+            foreach (var f in files)
+                Console.WriteLine(
+                    $"{f.Name}: {f.FileSize}, {f.Attributes}");
         }
     }
 }
