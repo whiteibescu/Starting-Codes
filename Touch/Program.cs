93eddb33ed0f9ab110bf45a03f81eafc5bce913e
Sean <whiteibescu@gmail.com>
2020-04-30 21:26:16 +0900

    작성미완료

---
+++ Touch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Touch
+{
+    class MainApp
+    {
+        static void OnWrongPathType(string type)
+        {
+            Console.WriteLine($"{type} is wrong type");
+            return;
+        }
+
+        static void Main
+    }
+}
