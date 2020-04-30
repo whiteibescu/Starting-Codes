9661554138109da71ad6d9ce66af7111d34bd4b1
Sean <whiteibescu@gmail.com>
2020-04-30 21:13:27 +0900

    DuckTyping

--- DuckTyping/Program.cs
+++ DuckTyping/Program.cs
@@ -25,5 +25,24 @@ namespace DuckTyping
         public void Swim()
         { Console.WriteLine("Robot.Swim"); }
 
+        public void Quack()
+        { Console.WriteLine("Robot.Quack"); }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] arr = new dynamic[] { new Duck(), new Mallard(), new Robot() };
+            foreach (dynamic duck in arr)
+            {
+                Console.WriteLine(duck.GetType());
+                duck.Walk();
+                duck.Swim();
+                duck.Quack();
+
+                Console.WriteLine();
+            }
+        }
     }
 }
