cd996ff2c482b93991e785b8a3de4a4faf165559
Sean <whiteibescu@gmail.com>
2020-04-30 21:17:12 +0900

    작성미완료

---
+++ Dir/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Dir
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
+
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine("- Directories : :");
+        }
+    }
+}
