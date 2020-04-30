18cd98e6adee919580b55c0dff9dfc4838153a3b
Sean <whiteibescu@gmail.com>
2020-04-30 21:09:39 +0900

    작성미완료

---
+++ DuckTyping/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DuckTyping
+{
+    class Duck
+    {
+        public void Walk()
+        { Console.WriteLine(this.GetType() + ".Walk"); }
+
+        public void Swim()
+        { Console.WriteLine(this.GetType() + ".Swim"); }
+
+        public void Quack()
+        { Console.WriteLine(this.GetType() + ".Quack"); }
+    }
+
+    class Mallard : Duck
+    { }
+
+    class Robot
+    {
+        public void Walk()
+        { Console.WriteLine("Robot.Walk"); }
+
+        public void Swim()
+        { Console.WriteLine("Robot.Swim"); }
+
+    }
+}
