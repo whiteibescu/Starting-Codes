49f13ebc4970998778a7ff4a4c425dc7327d2ad8
Sean <whiteibescu@gmail.com>
2020-05-01 13:55:42 +0900

    SerializingCollection

---
+++ SerializingCollection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializingCollection
+{
+    [Serializable]
+    class NameCard
+    {
+        public NameCard(string Name, string Phone, int Age)
+        {
+            this.Name = Name;
+            this.Phone = Phone;
+            this.Age = Age;
+        }
+
+        public string Name;
+        public string Phone;
+        public int Age;
+    }
+
+    class MainApp
+        static void Main(string[] args)
+    {
+        Stream ws = new FileStream("a.dat", FileMode.Create);
+        BinaryFormatter serializer = new BinaryFormatter().;
+    }
+}
