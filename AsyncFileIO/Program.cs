e587031e4eef560903826c153bdac34be19a1c14
Sean <whiteibescu@gmail.com>
2020-05-11 17:04:41 +0900

    AsyncFileIO

---
+++ AsyncFileIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncFileIO
+{
+    class MainApp
+    {
+        // 파일 복사 후 복사한 파일 용량 반환.
+        static async Task<long> CopyAsync(string FromPath, string ToPath)
+        {
+            using (
+                var fromStream = new FileStream(FromPath, FileMode.Open))
+            {
+                long totalCopied = 0;
+
+                using (
+                    )
+            }
+        }
+    }
+}
