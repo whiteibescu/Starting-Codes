ddd003fc01da09d56e23b350a3d64ac6cf2b099d
Sean Park <whiteibescu@gmail.com>
2022-04-17 22:15:35 +0900

    Decorator

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public abstract class Component
+    {
+        public abstract string Operation();
+    }
+
+    class ConcreteComponent : Component
+    {
+        public override string Operation()
+        {
+            return "ConcreteComponent";
+        }
+    }
+
+    abstract class Decorator : Component
+    {
+        protected Component _component;
+
+        public Decorator(Component component)
+        {
+            this._component = component;
+        }
+
+        public void SetComponent(Component component)
+        {
+            this._component = component;
+        }
+
+        public override string Operation()
+        {
+            if (this._component != null)
+            {
+                return this._component.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Component comp) : base(comp)
+        { 
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+
+    public class Client
+    {
+        public void ClientCode(Component component)
+        {
+            Console.WriteLine("Result: {0}", component.Operation());
+        }
+    }
+
+    class MainApp
+    {
+        public static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            var simple = new ConcreteComponent();
+            Console.WriteLine("Cleint: I get a simple component");
+            client.ClientCode(simple);
+            Console.WriteLine();
+        }
+    }
+}
