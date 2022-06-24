ca55401b94e78a949ea0a8f0549661fcb9a15362
Sean Park <whiteibescu@gmail.com>
2022-06-24 14:34:11 +0900

    DoFactory Design Pattern Factory Method

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,36 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DecoratorDesignPattern
+namespace DoFactory.GangOfFour.Factory.RealWorld
 {
-    public interface ICar
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Factory Method Design Pattern.
+    /// </summary>
+
+    class MainApp
     {
-        ICar ManufactureCar();
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+
+        static void Main()
+        {
+            // Note: constructors call Factory Method
+
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+
+            // Display document pages
+
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
     }
 
-    public class BMWCar : ICar
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+
+    abstract class Page
     {
-        private string CarName = "BMW";
-        public string CarBody { get; set; }
-        public string CarDoor { get; set; }
-        public string CarWheels { get; set; }
-        public string CarGlass { get; set; }
-        public string Engine { get; set; }
-        public override string ToString()
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class SkillsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class EducationPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class ExperiencePage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class IntroductionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class ResultsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class ConclusionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class SummaryPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+
+    class BibliographyPage : Page
+    {
+    }
+
+    /// <summary>
+    /// The 'Creator' abstract class
+    /// </summary>
+
+    abstract class Document
+    {
+        private List<Page> _pages = new List<Page>();
+
+        // Constructor calls abstract Factory method
+
+        public Document()
         {
-            return "BMWCar [CarName=" + CarName + ", CarBody=" + CarBody + ", CarDoor=" + CarDoor + ", CarWheels="
-                            + CarWheels + ", CarGlass=" + CarGlass + ", Engine=" + Engine + "]";
+            this.CreatePages();
         }
-        public ICar ManufactureCar()
+
+        public List<Page> Pages
         {
-            CarBody = "carbon fiber material";
-            CarDoor = "4 car doors";
-            CarWheels = "6 car glasses";
-            CarGlass = "4 MRF wheels";
-            return this;
+            get { return _pages; }
+        }
+
+        // Factory Method
+
+        public abstract void CreatePages();
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+
+    class Resume : Document
+    {
+        // Factory Method implementation
+
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+
+    class Report : Document
+    {
+        // Factory Method implementation
+
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
         }
     }
 }
